8e96e205d50826ea33a612ced1096c038d0c08c9
Ayanoo <gagan270903@gmail.com>
2026-01-10 12:34:51 +0000

    clean the code

--- components/login-form.jsx
+++ components/login-form.jsx
@@ -1,22 +1,13 @@
 "use client"
 import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { signinAction } from "@/lib/actions/user-auth"
-// import { signIn } from "@/lib/auth"
 import { cn } from "@/lib/utils"
 import { useForm } from "@tanstack/react-form"
 import { Loader2 } from "lucide-react"
-import { useActionState, useTransition } from "react"
-import { Field, FieldError, FieldGroup, FieldLabel } from "./ui/field"
 import { signIn } from "next-auth/react"
+import { useTransition } from "react"
+import { Field, FieldError, FieldLabel } from "./ui/field"
 
 
 export function LoginForm({
@@ -24,9 +15,6 @@ export function LoginForm({
   ...props
 }) {
   const [isPending, startTransition] = useTransition()
-
-
-  // const [state, formAction, isPending] = useActionState(signinAction, { error: "", status: "INITIAL" })
 
 
   const form = useForm({
@@ -52,9 +40,6 @@ export function LoginForm({
           </CardDescription>
         </CardHeader>
         <CardContent>
-
-
-
           <form
             onSubmit={(e) => {
               e.preventDefault()
@@ -111,37 +96,6 @@ export function LoginForm({
               )}
             </Button>
           </form>
-
-
-
-
-
-
-
-          {/* <form action={formAction} >
-            <div className="flex flex-col gap-6">
-              <div className="grid gap-3">
-                <Label htmlFor="email">Email</Label>
-                <Input id="email" type="email" name="email" placeholder="m@example.com" autoComplete="email" required />
-              </div>
-              <div className="grid gap-3">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
-                  <a
-                    href="#"
-                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline">
-                    Forgot your password?
-                  </a>
-                </div>
-                <Input id="password" type="password" name="password" autoComplete="current-password" required />
-              </div>
-              <div className="flex flex-col gap-3">
-                <Button type="submit" className="w-full" disabled={isPending}>
-                  {isPending ? <> <Loader2 className="animate-spin" /> </> : "Login"}
-                </Button>
-              </div>
-            </div>
-          </form> */}
         </CardContent>
       </Card>
     </div>
